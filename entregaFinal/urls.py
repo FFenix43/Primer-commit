cc65b115f7eeef1f247c026ab5154269bae0023e
FFenix43 <tomalvarengam@gmail.com>
2022-12-13 20:13:00 -0300

    actualizacion

--- entregaFinal/urls.py
+++ entregaFinal/urls.py
@@ -1,2 +1,11 @@
 from django.contrib import admin 
-from django.urls import path 
+from django.urls import path
+from LOTR import views
+
+urlpatterns = [
+
+    path("", views.razas_index, name="raza-index")
+]
+
+
+
